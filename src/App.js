5b6c666c33b1d8bf47b615ec4790c9e152f299eb
github username <samson.yuwono@gmail.com>
2020-09-16 23:16:45 -0400

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    const [item] = data.results;
+    setData(item);
+    setLoading(false);
+  }, []);
+
+  return { data, loading };
+};
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const { data, loading } = useFetch(
+    "http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]"
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>You click {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click Me!</button>
+      {loading ? <div>loading...</div> : <div>{data}</div>}
     </div>
   );
 }
