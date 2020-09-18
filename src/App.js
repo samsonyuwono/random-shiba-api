d0f39e8864cab2b4a1492ec81d3ac6232b0faa61
github username <samson.yuwono@gmail.com>
2020-09-17 22:50:33 -0400

    fetching data from shibe online api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
+const API_URL = "http://shibe.online/api/shibes?count=1";
+const CORS_ANYWHERE = "https://cors-anywhere.herokuapp.com/";
+
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  useEffect(async () => {
-    const response = await fetch(url);
-    const data = await response.json();
-    const [item] = data.results;
-    setData(item);
-    setLoading(false);
+  useEffect(() => {
+    axios
+      .get(CORS_ANYWHERE + API_URL)
+      .then((response) => {
+        console.log(response);
+
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
 
   return { data, loading };
@@ -18,14 +28,18 @@ const useFetch = (url) => {
 function App() {
   const [count, setCount] = useState(0);
 
-  const { data, loading } = useFetch(
-    "http://shibe.online/api/shibes?count=[1-100]&urls=[true/false]&httpsUrls=[true/false]"
-  );
+  const { data, loading } = useFetch("http://shibe.online/api/shibes?count=1");
   return (
     <div className="App">
       <p>You click {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click Me!</button>
-      {loading ? <div>loading...</div> : <div>{data}</div>}
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div>
+          <img src={data} alt="shibe" />
+        </div>
+      )}
     </div>
   );
 }
